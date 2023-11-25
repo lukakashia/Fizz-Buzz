4f8b7aa600f2f92dd0db83684bcbc32c6e1928b4
DESKTOP-9FT8NSD\THERE WAS PIPIKO <lukakashia10@gmail.com>
2023-11-24 19:53:20 -0800

    FizzBuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Buzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a number: ");
+            int a;
+            a = Convert.ToInt32(Console.ReadLine());
+
+            if(a >= 1 && a <= 100)
+            {
+                if(a % 3 == 0)
+                {
+                    Console.Write("Fizz");
+                }
+                if(a % 5 == 0)
+                {
+                    Console.Write("Buzz");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong number!");
+            }
+        }
+    }
+}
